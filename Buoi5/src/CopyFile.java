62ee5024e9e6be7be9b1206926bf321dd319a636
pdkhanh0509@gmail.com <pdkhanh0509@gmail.com>
2024-03-16 20:30:24 +0700

    update

--- Buoi5/src/CopyFile.java
+++ Buoi5/src/CopyFile.java
@@ -43,54 +43,57 @@ public class CopyFile {
             e.printStackTrace();
         }
     }
-
-    public static void copyFile(String src, String dest) {
-        // Tao ra mot cai file moi
-        // Tung buoc doc du lieu tu file cu => ghi sang file moi
-        File fileSrc = new File(src); // 1
-        File fileDest = new File(dest);// 2
-        try {
-            if (!fileSrc.exists()) {
-                System.out.println("src khong ton tai!");
-            } else {
-                if (fileDest.exists()) {
-                    System.out.println("File dest da ton tai, overwrite? (y/n)");
-                    Scanner sc = new Scanner(System.in);
-                    String c = sc.nextLine();
-                    if (c.equals("n")) {
-                        return;
-                    }
+        public static void copyFile(String src, String dest) {
+            // Tao ra mot cai file moi
+            // Tung buoc doc du lieu tu file cu => ghi sang file moi
+            File fileSrc = new File(src); // 1
+            File fileDest = new File(dest);// 2
+            try {
+                if (!fileSrc.exists()) {
+                    System.out.println("File src khong ton tai!");
                 } else {
-                    fileDest.createNewFile();
+                    if (fileDest.exists()) {
+                        System.out.println("File dest da ton tai, ban co muon ghi de hay khong (y/n)?");
+                        Scanner sc = new Scanner(System.in);
+                        String c = sc.nextLine();
+                        if (c.equals("n")) { // Neu khong muon ghi de thi ngung chuong trinh
+                            return;
+                        }
+                    } else {
+                        fileDest.createNewFile();
+                    }
+
+                    // Tao stream de doc du lieu
+                    FileInputStream fis = new FileInputStream(fileSrc);
+
+                    // Tao stream ghi du lieu
+                    FileOutputStream fos = new FileOutputStream(fileDest);
+
+                    // Copy tung byte
+                    int b = 0;
+                    while (true) {
+                        b = fis.read(); // Doc len mot byet
+
+                        if (b == -1) break; // =-1 khi khong co du lieu de doc
+
+                        fos.write(b); // ghi xuong file
+                        fos.flush(); // ep du lieu duoc ghi xuong file ngay lap tuc
+                    }
+                    fis.close();
+                    fos.close();
                 }
-                FileInputStream fis = new FileInputStream(fileSrc);
-                FileOutputStream fos = new FileOutputStream(fileDest);
-
-                // Copy tung byte
-                int b = 0;
-                while(true){
-                    b = fis.read(); // Doc len mot byet
-
-                    if(b==-1) break; // =-1 khi khong co du lieu de doc
-
-                    fos.write(b); // ghi xuong file
-                    fos.flush(); // ep du lieu duoc ghi xuong file ngay lap tuc
-                }
-                fis.close();
-                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+
+        public static void main(String[] args) {
+            String src = "E:\\temp\\Test\\ABC.txt";
+            String dest = "E:\\temp\\Test\\New folder\\ABC.txt";
+            copyFile(src, dest);
+            //copyFileArr(src,dest);
         }
     }
-
-    public static void main(String[] args) {
-        String src = "E:\\temp\\Test\\ABC.txt";
-        String dest = "E:\\temp\\Test\\New folder\\ABC.txt";
-        //copyFile(src,dest);
-        copyFileArr(src,dest);
-    }
-}
 
 
 
